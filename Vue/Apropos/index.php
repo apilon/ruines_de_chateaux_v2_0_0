4994331f903bf33de4f31251089525a2135a6d75
apilon <apilon@192.168.2.27>
2020-05-11 09:34:46 -0400

    Dépôt initial

---
+++ Vue/Apropos/index.php
@@ -0,0 +1,46 @@
+<?php $titre = 'Le Blogue du prof version 1.0.0.1'; ?>
+
+<ul>
+    <li>André Pilon</li>
+
+    <li>420-4A4 MO Web et Bases de données</li>
+    <li>Hiver 2020, Collège Montmorency</li>
+</ul>
+<h3>Ruines de châteaux</h3>
+<ul>
+    <li>L'application "Ruines de châteaux" permet de présenter la liste des ruines de châteaux à visiter dans une région donnée. En Tchéquie par exemple.</li>
+    <li>La page d'Accueil présente la liste des sites de ruines
+        avec l'inscripteur et le prix :</li>
+    <ul>
+        <li>Cette version n'offre pas encore la gestion des inscripteurs. Il
+            n'y a qu'un seul inscripteur pour l'instant. <br>
+        </li>
+    </ul>
+
+    <li>Les visiteurs peuvent cliquer sur le nom d'un
+        site de ruine pour lire la descripton complète du site :<br>
+    </li>
+    <ul>
+        <li>À la suite de la description du site on présente la lilste des autour points d'intérêt près du site ;</li>
+    </ul>
+</ul>
+<br>
+
+<table>
+    <tr>
+        <td>
+            <h4>Base de données utilisée par l'application :</h4>
+            <object data="Contenu/images/ruines_de_chateaux.svg" type="image/svg+xml" height="300" width="800">
+                <img src="Contenu/images/bd_ruines_de_chateaux.png" alt=""/>
+            </object><br/>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h4>Basé sur ce modèle original :</h4>
+            <a href="http://www.databaseanswers.org/data_models/hotels_and_tourist_attractions/index.htm" >
+                <img src="Contenu/images/DataModelAttractions.png" alt=""/><br/>
+                Ruines as a "Real Life example :</a>
+        </td>
+    </tr>
+</table>
